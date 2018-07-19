a4ee5c25f91e7ce902c5d2fac94b9aa183b7d186
jimskon <jamesskon@gmail.com>
2018-07-19 15:24:35 -0400

    Update Node.h

--- Node.h
+++ Node.h
@@ -6,6 +6,8 @@
  */
 #ifndef NODE_H
 #define NODE_H
+#include <climits>
+#include <list>
 #include "Point.h"
 
 using namespace std;
@@ -15,11 +17,9 @@ static int trans2[] = {0, 1, 2, -2, 3};
 class Node {
 public:
     Node();
-    Node(Point p);
-    //Node(const Node& orig);
-    //Node(Node const &) = delete;
-    //void operator=(Node) = delete;
-
+    
+    Node(int c, list<int> s, Point p);
+    
     virtual ~Node();
 
     void display();
@@ -36,21 +36,24 @@ public:
     
     void setColor(int c);
     int getColor();
-    
-    string retreiveSig();
-    
-    vector<int> getSig();
+        
     vector<int> resize(int l);
     
-    void copySig(vector<int> v);
-    void pushSig(int v);
+    int sizeSig();
+    list<int> getSig();
     
-    int size();
+    void pushSigBack(int v);
+    void pushSigBack2(int x, int y);
+    
+    string listToString();
 
 private:
 
     vector<int> signature;
-    vector<int> neighbors;
+    vector<int> neighbors;  
+    
+    list<int> sigLis;
+    
     Point p;
     int color;
     int dist;
